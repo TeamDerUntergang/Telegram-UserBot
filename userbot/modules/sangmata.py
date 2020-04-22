b5ecf1de9b77c1c14f67255fad86cf9488eeaad9
NaytSeyd <ahackgz@gmail.com>
2020-04-22 23:31:46 +0300

    seden: Add Sangmata Module

---
+++ userbot/modules/sangmata.py
@@ -0,0 +1,50 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2020 TeamDerUntergang.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+
+
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot.events import register
+from userbot import bot, CMD_HELP
+
+@register(outgoing=True, pattern="^.sangmata(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("`Reply to any user message.`")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("```reply to text message```")
+       return
+    chat = "@SangMataInfo_bot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("`Reply to actual users message.`")
+       return
+    await event.edit("`Processing`")
+    async with bot.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              await bot.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("`Please unblock @sangmatainfo_bot and try again`")
+              return
+          if response.text.startswith("Forward"):
+             await event.edit("`can you kindly disable your forward privacy settings for good?`")
+          else: 
+             await event.edit(f"{response.message.message}")
+
+
+CMD_HELP.update({
+        "sangmata": 
+        ".sangmata \
+          \nUsage: View user history.\n"
+    })

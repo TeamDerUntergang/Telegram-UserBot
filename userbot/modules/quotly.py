a8d99baabf9c6e1a62533b76b4fc552d9e493044
frknkrc44 <krc440002@gmail.com>
2020-04-22 22:38:20 +0300

    seden: Fix for quotly

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -16,31 +16,39 @@ async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
+       await event.edit("`Reply to any user message.`")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.edit("```Reply to text message```")
+       await event.edit("`Reply to text message`")
        return
     chat = "@QuotLyBot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
+       await event.edit("`Reply to actual users message.`")
        return
-    await event.edit("```Making a Quote```")
-    async with bot.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
-              await bot.forward_messages(chat, reply_message)
-              response = await response 
+    await event.edit("`Making a Quote`")
+
+    async with bot.conversation(chat, exclusive=False, replies_are_responses=True) as conv:
+          response = None
+          try:
+              msg = await reply_message.forward_to(chat)
+              response = await conv.get_response(message=msg, timeout=5)
           except YouBlockedUserError: 
-              await event.reply("```Please unblock @QuotLyBot and try again```")
+              await event.edit("`Please unblock @QuotLyBot and try again`")
               return
-          if response.text.startswith("Hi!"):
-             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
+          except Exception as e:
+              print(e.__class__)
+
+          if not response:
+              await event.edit("`I can't get any response from bot!`")
+          elif response.text.startswith("Hi!"):
+             await event.edit("`Can you kindly disable your forward privacy settings for good?`")
           else: 
-             await event.delete()   
-             await bot.forward_messages(event.chat_id, response.message)
+             await event.delete()
+             await response.forward_to(event.chat_id)
+          await conv.mark_read()
+          await conv.cancel_all()
 
 CMD_HELP.update({
         "quotly": 

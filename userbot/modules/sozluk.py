0f666da1f066dab216959d285210e0f4e8e0847c
Quiec <46392667+Quiec@users.noreply.github.com>
2020-04-27 18:10:18 +0300

    Create sozluk.py

---
+++ userbot/modules/sozluk.py
@@ -0,0 +1,62 @@
+"""
+Turkish word meaning. Only Turkish. Coded @By_Azade, Seden uyarlaması @qulec
+"""
+import requests
+
+from userbot import CMD_HELP
+from userbot.events import register
+from userbot.modules.admin import get_user_from_event
+
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+
+
+def searchTureng_tr(word):
+    url="https://tureng.com/tr/turkce-ingilizce/"+word
+    try:
+        answer =  requests.get(url)
+    except:
+        return "No connection"
+    soup = BeautifulSoup(answer.content, 'html.parser')
+    trlated='{} Kelimesinin Anlamı/Anlamları:\n\n'.format(word)
+    try:
+        table = soup.find('table')
+        td = table.find_all('td', attrs={'lang':'en'})
+        # print(td)
+        for val in td[0:5]:
+            trlated = '{}👉  {}\n'.format(trlated , val.text )
+        return trlated
+    except:
+        return "Sonuç bulunamadı"
+
+@register(outgoing=True, pattern="^.tureng ?(.*)")
+async def tureng(event): 
+    input_str = event.pattern_match.group(1)
+    result = searchTureng_tr(input_str)
+    await event.edit(result)
+
+@register(outgoing=True, pattern="^.tdk ?(.*)")
+async def tdk(event): 
+    if event.fwd_from:
+        return
+    inp = event.pattern_match.group(1)
+    kelime = "https://sozluk.gov.tr/gts?ara={}".format(inp)
+    headers = {"USER-AGENT": "Seden"}
+    response = requests.get(kelime, headers=headers).json()
+    
+    try:
+        anlam_sayisi = response[0]['anlam_say']
+        x = "TDK Sözlük\n\nKelime: **{}**\n".format(inp)
+        for anlamlar in range(int(anlam_sayisi)):
+            x += "👉{}\n".format(response[0]['anlamlarListe'][anlamlar]['anlam'])
+            # print(x)
+        await event.edit(x)
+    except KeyError:
+        await event.edit(KeyError)
+
+CMD_HELP.update({
+    "sozluk":
+    ".tdk <kelime> .\
+    \nVerdiğiniz kelimeyi TDK Sözlükte arar.\n\n.tureng <kelime> .\
+    \nVerdiğiniz kelimeyi Tureng Sözlükte arar."
+})

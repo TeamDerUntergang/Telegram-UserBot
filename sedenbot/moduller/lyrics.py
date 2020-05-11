da2dde0c89f772ea640182fd9036d0c8d3f49016
NaytSeyd <NaytSeyd@yandex.com>
2020-05-11 23:42:28 +0300

    seden: Release v3.0
    Co-authored-by: frknkrc44 <krc440002@gmail.com>
    Signed-off-by: NaytSeyd <NaytSeyd@yandex.com>

---
+++ sedenbot/moduller/lyrics.py
@@ -0,0 +1,78 @@
+# Copyright (C) 2020 TeamDerUntergang.
+#
+# SedenUserBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SedenUserBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import os
+import random
+import lyricsgenius
+
+from sedenbot import CMD_HELP, LOGS, GENIUS
+from sedenbot.events import sedenify
+
+@sedenify(outgoing=True, pattern="^.lyrics(?: |$)(.*)")
+async def lyrics(lyric):
+    if r"-" in lyric.text:
+        pass
+    else:
+        await lyric.edit("`Hata: lütfen <sanatçı> ve <şarkı> için bölücü olarak '-' kullanın`\n"
+                         "Örnek: `Stabil - Reenkarne`")
+        return
+
+    if GENIUS is None:
+        await lyric.edit(
+            "`Lütfen Genius tokeni ayarlayınız. Teşekkürler!`")
+    else:
+        genius = lyricsgenius.Genius(GENIUS)
+        try:
+            args = lyric.text.split('.lyrics')[1].split('-')
+            artist = args[0].strip(' ')
+            song = args[1].strip(' ')
+        except:
+            await lyric.edit("`Lütfen sanatçı ve şarkı ismini veriniz`")
+            return
+
+    if len(args) < 1:
+        await lyric.edit("`Lütfen sanatçı ve şarkı ismini veriniz`")
+        return
+
+    await lyric.edit(f"`{artist} - {song} için şarkı sözleri aranıyor...`")
+
+    try:
+        songs = genius.search_song(song, artist)
+    except TypeError:
+        songs = None
+
+    if songs is None:
+        await lyric.edit(f"Şarkı **{artist} - {song}** bulunamadı!")
+        return
+    if len(songs.lyrics) > 4096:
+        await lyric.edit("`Şarkı sözleri çok uzun, görmek için dosyayı görüntüleyin.`")
+        with open("lyrics.txt", "w+") as f:
+            f.write(f"Arama sorgusu: \n{artist} - {song}\n\n{songs.lyrics}")
+        await lyric.client.send_file(
+            lyric.chat_id,
+            "lyrics.txt",
+            reply_to=lyric.id,
+        )
+        os.remove("lyrics.txt")
+    else:
+        await lyric.edit(f"**Arama sorgusu**: \n`{artist} - {song}`\n\n```{songs.lyrics}```")
+    return
+
+CMD_HELP.update({
+    "lyrics":
+    "Kullanım: .`lyrics <sanatçı adı> - <şarkı ismi>`\n"
+    "NOT: ""-"" ayracı önemli!"
+})
---
+++ sedenbot/moduller/system.py
@@ -0,0 +1,143 @@
+# Copyright (C) 2020 TeamDerUntergang.
+#
+# SedenUserBot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SedenUserBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+""" Sunucu hakkında bilgi veren UserBot modülüdür. """
+
+from asyncio import create_subprocess_shell as asyncrunapp
+from asyncio.subprocess import PIPE as asyncPIPE
+from platform import python_version, uname
+from shutil import which
+from os import remove
+from telethon import version
+
+from sedenbot import CMD_HELP
+from sedenbot.events import sedenify
+# ================= CONSTANT =================
+DEFAULTUSER = uname().node
+# ============================================
+@sedenify(outgoing=True, pattern="^.neofetch$")
+async def sysdetails(sysd):
+    """ .neofetch komutu neofetch kullanarak sistem bilgisini gösterir. """
+    try:
+        neo = "neofetch --stdout"
+        fetch = await asyncrunapp(
+            neo,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+
+        stdout, stderr = await fetch.communicate()
+        result = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+
+        await sysd.edit("`" + result + "`")
+    except FileNotFoundError:
+        await sysd.edit("`Öncelikle neofetch modülünü yükleyin !!`")
+
+@sedenify(outgoing=True, pattern="^.botver$")
+async def bot_ver(event):
+    """ .botver komutu bot versiyonunu gösterir. """
+    if which("git") is not None:
+        invokever = "git describe --all --long"
+        ver = await asyncrunapp(
+            invokever,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+        stdout, stderr = await ver.communicate()
+        verout = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+
+        invokerev = "git rev-list --all --count"
+        rev = await asyncrunapp(
+            invokerev,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+        stdout, stderr = await rev.communicate()
+        revout = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+
+        await event.edit("[Seden UserBot](https://telegram.dog/SedenUserBot) `Versiyonu: "
+                         f"{verout} v3.0"
+                         "` \n"
+                         "`Toplam değişiklik: "
+                         f"{revout}"
+                         "`")
+    else:
+        await event.edit(
+            "Bu arada Seden seni çok seviyor. ❤"
+        )
+
+@sedenify(outgoing=True, pattern="^.pip(?: |$)(.*)")
+async def pipcheck(pip):
+    """ .pip komutu python-pip araması yapar. """
+    pipmodule = pip.pattern_match.group(1)
+    if pipmodule:
+        await pip.edit("`Aranıyor . . .`")
+        invokepip = f"pip3 search {pipmodule}"
+        pipc = await asyncrunapp(
+            invokepip,
+            stdout=asyncPIPE,
+            stderr=asyncPIPE,
+        )
+
+        stdout, stderr = await pipc.communicate()
+        pipout = str(stdout.decode().strip()) \
+            + str(stderr.decode().strip())
+
+        if pipout:
+            if len(pipout) > 4096:
+                await pip.edit("`Çıktı çok büyük, dosya olarak gönderiliyor.`")
+                file = open("output.txt", "w+")
+                file.write(pipout)
+                file.close()
+                await pip.client.send_file(
+                    pip.chat_id,
+                    "output.txt",
+                    reply_to=pip.id,
+                )
+                remove("output.txt")
+                return
+            await pip.edit("**Sorgu: **\n`"
+                           f"{invokepip}"
+                           "`\n**Sonuç: **\n`"
+                           f"{pipout}"
+                           "`")
+        else:
+            await pip.edit("**Sorgu: **\n`"
+                           f"{invokepip}"
+                           "`\n**Sonuç: **\n`Bir şey bulunamadı.`")
+    else:
+        await pip.edit("`Bir örnek görmek için .seden pip komutunu kullanın.`")
+
+@sedenify(outgoing=True, pattern="^.alive$")
+async def amialive(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("`Merhaba Seden! Seni Seviyorum ❤`")
+
+CMD_HELP.update(
+    {"neofetch": ".neofetch\
+    \nKullanım: Neofetch komutunu kullanarak sistem bilgisi gösterir."})
+CMD_HELP.update({"botver": ".botver\
+    \nKullanım: UserBot sürümünü gösterir."})
+CMD_HELP.update(
+    {"pip": ".pip <module(s)>\
+    \nKullanım: Pip modüllerinde arama yapar."})
+CMD_HELP.update({
+    "alive": ".alive\
+    \nKullanım: Seden botunun çalışıp çalışmadığını kontrol etmek için kullanılır."
+})
